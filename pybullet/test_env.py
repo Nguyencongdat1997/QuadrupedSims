dcfe910875909050aea276730fce8574fded5f2b
GattoMac <traceformula2000@gmail.com>
2022-01-14 09:23:09 -0800

    Pybullet Env | Test environment

--- pybullet/test_env.py
+++ pybullet/test_env.py
@@ -8,11 +8,12 @@ for ep in range(1):
     total_reward = 0
     for time_step in range(1000):
         env.render()
-        #print(observation)
+        # print(observation)
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
         total_reward += reward
         if done:
-            break
+            env.reset()
+            total_reward = 0
     print("Episode {} finished after {} timesteps with reward {}".format(ep+1, time_step+1, total_reward))
 env.close()
